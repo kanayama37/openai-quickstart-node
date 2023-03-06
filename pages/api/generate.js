07eb7f510577757908fed0f7d3304fed49238980
y-temp4 <y.temp4@gmail.com>
2023-03-06 16:36:39 +0900

    Update for gpt-3.5-turbo

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,30 +9,41 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
+  const animal = req.body.animal || "";
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
-      }
+      },
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "Suggest three names for an animal that is a superhero.",
+        },
+        { role: "user", content: animal },
+      ],
+      // prompt: generatePrompt(animal),
       temperature: 0.6,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+    console.log(completion.data);
+    res
+      .status(200)
+      .json({ result: completion.data.choices[0].message.content });
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,8 +52,8 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
